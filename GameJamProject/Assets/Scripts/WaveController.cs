cf735bd3ab02be6975679c9744df06c692bbb66c
Débora Beda <deborabeda@live.com>
2021-08-22 11:02:32 -0300

    Wave movement

--- GameJamProject/Assets/Scripts/WaveController.cs
+++ GameJamProject/Assets/Scripts/WaveController.cs
@@ -7,15 +7,29 @@ public class WaveController : MonoBehaviour
     MeshRenderer mr;
     [SerializeField] float speed;
 
+    // User Inputs
+    float amplitude = 0.05f;
+    float frequency = 1f;
+
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
     // Start is called before the first frame update
     void Start()
     {
         mr = GetComponent<MeshRenderer>();
+        posOffset = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
         mr.material.mainTextureOffset += Vector2.right * speed * Time.deltaTime;
+
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
     }
 }
